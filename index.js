2f8f310816fa97f8346233dcb33eead0261b21b6
Taloleamit <talole84@gmail.com>
2023-03-31 18:50:59 +0530

    updae

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.get('/', function (req, res) {
 app.use(cors());
 app.use(express.json());
 
-app.post('/uploadFile', function (req, res) {
-    res.send('www.google.com')
+app.get('/test', function (req, res) {
+    res.send('https://github.com/amit-talole')
   })
 
 
-app.listen(3000,(req,res)=>{
+app.listen(8080,(req,res)=>{
   console.log(`3000 Port Activate successfully `);
 })
